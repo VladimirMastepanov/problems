2d251ca55d100ca830de564f9abe7272c05fc7c7
Vladimir Mastepanov <146621116+VladimirMastepanov@users.noreply.github.com>
2024-08-16 13:33:54 +0200

    Create index.js

---
+++ base/math/index.js
@@ -0,0 +1,8 @@
+import cube from './numbers1';
+import multi, { pow2 as pow, sum2 as sum, sub2 as sub } from './numbers2';
+import sqrt from './numbers3';
+
+export {
+  multi, pow, sum, sub, sqrt,
+};
+export default cube;
