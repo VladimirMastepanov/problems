6908458669619d88a57a73b9a9bfa012891738a9
Vladimir Mastepanov <146621116+VladimirMastepanov@users.noreply.github.com>
2024-09-24 10:53:04 +0200

    Update application.js

--- DOM-API/progress/src/application.js
+++ DOM-API/progress/src/application.js
@@ -1,1 +1,10 @@
-
+export default () => {
+  const element = document.querySelector('progress');
+  const handler = (counter = 0) => {
+    if (counter <= element.getAttribute('max')) {
+      element.setAttribute('value', counter);
+      setTimeout(() => handler(counter + 1), 1000);
+    }
+  };
+  handler();
+};
