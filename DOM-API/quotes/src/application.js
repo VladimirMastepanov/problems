715f64a57125d63f590ca43f74be93f1f4f38c4a
Vladimir Mastepanov <146621116+VladimirMastepanov@users.noreply.github.com>
2024-09-26 17:56:24 +0200

    Update application.js

--- DOM-API/quotes/src/application.js
+++ DOM-API/quotes/src/application.js
@@ -1,1 +1,14 @@
+import axios from 'axios';
 
+const routes = {
+  randomQuotePath: () => '/api/quotes/random',
+};
+
+export default () => {
+  const button = document.querySelector('button');
+  const result = document.querySelector('#result');
+  button.addEventListener('click', async () => {
+    const response = await axios.get(routes.randomQuotePath());
+    result.textContent = response.data.quote;
+  });
+};
