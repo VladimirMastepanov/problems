cbbbdee76d3e358b57a552be00cca2eae2814e3e
Vladimir Mastepanov <146621116+VladimirMastepanov@users.noreply.github.com>
2024-10-07 14:12:51 +0200

    Update solution.ts

--- TS/errorHandler/solution.ts
+++ TS/errorHandler/solution.ts
@@ -1,1 +1,33 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  errorHandler,
+  appErrorHandler,
+  customErrorHandler,
+} from './handlers';
 
+import AppError from './errors/AppError';
+import CustomError from './errors/CustomError';
+
+// BEGIN (write your solution here)
+
+function errorCatcher(error: Error): string;
+function errorCatcher(error: AppError): string;
+function errorCatcher(error: CustomError): string;
+
+function errorCatcher(error: any): string {
+  if (error instanceof AppError) {
+    return appErrorHandler(error);
+  }
+  
+  if (error instanceof CustomError) {
+    return customErrorHandler(error);
+  }
+  
+  if (error instanceof Error) {
+    return errorHandler(error);
+  }
+  
+  throw error;
+}
+
+export default errorCatcher;
